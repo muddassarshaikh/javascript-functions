e4a2d5e6865735878a48001cec686b6207897d85
Ayesha Shaikh <shaikh.muddassar8@gmail.com>
2019-01-01 00:35:42 +0530

    Complete new functionality

--- missingNumber.js
+++ missingNumber.js
@@ -1,10 +1,13 @@
 /**
  * Title - Find the Missing Number
+ * 
  * Description - Create a function that takes an array of numbers between 1 and 10 
  *               (excluding one number) and returns the missing number.
+ * 
  * Example - [1, 2, 3, 4, 6, 7, 8, 9, 10] ➞ 5
  *           [7, 2, 3, 6, 5, 9, 1, 4, 8] ➞ 10
  *           [10, 5, 1, 2, 4, 6, 8, 3, 9] ➞ 7
+ * 
  * Notes - The array of numbers will be unsorted (not in order).
  *         Only one number will be missing.
  */
