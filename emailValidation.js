5c7570341b772667e0ab3735125210ed023490d9
Muddassar Shaikh <muddassarshaikh@winjit.com>
2019-01-10 11:24:43 +0530

    Added email validation function

---
+++ emailValidation.js
@@ -0,0 +1,29 @@
+/**  
+ * Title - Basic E-Mail Validation
+ * 
+ * Description - Create a function that accepts a string, checks if it's a valid email address and 
+ *               returns either true or false, depending on the evaluation.
+ *               The string must contain an @ character.
+ *               The string must contain a . character.
+ *               The @ must have at least one character in front of it.
+ *               e.g. "e@edabit.com" is valid while "@edabit.com" is invalid.
+ *               The . and the @ must be in the appropriate places.
+ *               e.g. "hello.email@com" is invalid while "john.smith@email.com" is valid.
+ *               If the string passes these tests, it's considered a valid email address.
+ * 
+ * Examples - validateEmail("@gmail.com") ➞ false
+ *            validateEmail("hello.gmail@com") ➞ false
+ *            validateEmail("gmail") ➞ false
+ *            validateEmail("hello@gmail") ➞ false
+ *            validateEmail("hello@edabit.com") ➞ true
+ * 
+ * Notes - Check the Tests tab to see exactly what's being evaluated. 
+ *         You can easily solve this with regex but the challenge is intended to be solved with logic.
+ */
+
+function validateEmail(str) {
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  	return re.test(str);
+}
+
+console.log(validateEmail("@gmail.com"));
