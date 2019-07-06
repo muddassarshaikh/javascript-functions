ecdee4a56776e0ef7b43617784eada84734af8f4
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2019-07-06 11:16:13 +0530

    change in prime number

--- primeNumber.js
+++ primeNumber.js
@@ -13,4 +13,21 @@ const primeNumber = (number) => {
   return 'Prime Number';
 }
 
-console.log(primeNumber(17));
+const primeNumberUsingRecursion = (number, index, count) => {
+  if (count > 0) {
+    return 'Not Prime Number';
+  }
+  else if (number === index) {
+    return 'Prime Number';
+  }
+  else if (number % index === 0) {
+    count++;
+    return primeNumberUsingRecursion(number, ++index, count);
+  }
+  else {
+    return primeNumberUsingRecursion(number, ++index, count);
+  }
+}
+
+console.log(primeNumber(21));
+console.log(primeNumberUsingRecursion(21, 2, 0));
