f4bbc4b66826596e4ef1ec2730d838bb86c59797
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2019-07-12 12:09:03 +0530

    Added insertion Sorting

---
+++ insertionsort.js
@@ -0,0 +1,14 @@
+const insertionSort = (array) => {
+  for ( i=1; i<array.length; i++) {
+    let k = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > k ) {
+      array[j+1] = array[j];
+      j--;
+    }
+    array[j+1] = k;
+  }
+  return array;
+};
+
+console.log(insertionSort([12, 54,23, 1, 67,43, 2]))
