0050488ed5b70b59fce3ea6c32250cdbf1938736
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2019-07-23 14:36:43 +0530

    Added new function for calling cmd command from nodejs

---
+++ executionCMDCommandFromNode.js
@@ -0,0 +1,3 @@
+let exec = require("child_process").exec;
+let command = `mongorestore -d ${database} -c icdcodes /home/muddassar/Projects/Doxiva/dump/icdcodes.bson`;
+exec(command, (err, stdout, stderr) => {});
