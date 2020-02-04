3f91bec769860300239cd4f946c89ace157c8171
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2020-02-04 07:04:32 +0000

    Combine searching algorithm in one file

--- searching.js
+++ searching.js
@@ -1,3 +1,20 @@
+/**
+ * Linear Search
+ */
+const linearSearch = (array, index, searchnumber) => {
+  if (array[index] === searchnumber) {
+    return index;
+  } else if (array.length <= index) {
+    return 'Not Found';
+  } else {
+    return linearSearch(array, ++index, searchnumber);
+  }
+};
+console.log(linearSearch([23, 54, 23, 44, 345, 7], 0, 44));
+
+/**
+ * Binary Search
+ */
 const sort = arr => {
   return arr.sort((a, b) => a - b);
 };
@@ -18,4 +35,4 @@ const binarySearch = (array, low, high, searchnumber) => {
 };
 
 const array = sort([12, 24, 32, 45, 77]);
-console.log(binarySearch(array, 0, array.length, 12));
+console.log(binarySearch(array, 0, array.length, 45));
