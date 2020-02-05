fba08e1c4d1b3f8e9367a89802b30d440c74b0ca
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2020-02-05 09:51:38 +0000

    Changes made in frequent number functionality

--- frequentNumberFromArray.js
+++ frequentNumberFromArray.js
@@ -3,9 +3,9 @@
  * Write a method that takes an array of integers and returns an interger.
  * {1, 2, 3, 3} => 3
  * {1, 2, 3 } => 1 {or 2 or 3}
- * {3, 1, 4, 57, 4} => 4
+ * {3, 1, 3, 4, 57, 4} => 4
  */
-
+let max = [0, 0];
 const frequentNumber = array => {
   const numberObject = {};
   for (let i = 0; i < array.length; i++) {
@@ -14,18 +14,13 @@ const frequentNumber = array => {
     } else {
       numberObject[array[i]] = 1;
     }
-  }
 
-  let value = 0;
-  let number = 0;
-
-  for (let keys in numberObject) {
-    if (value < numberObject[keys]) {
-      value = numberObject[keys];
-      number = keys;
+    if (max[1] < numberObject[array[i]]) {
+      max[0] = array[i];
+      max[1] = numberObject[array[i]];
     }
   }
-  return number;
+  return max[0];
 };
 
-console.log(frequentNumber([3, 1, 4, 57, 4]));
+console.log(frequentNumber([3, 1, 4, 57, 4, 1, 1]));
