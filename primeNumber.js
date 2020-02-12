b228afdb4d69eb0297b7744ed086f32cc5032958
Muddassar S Shaikh <shaikh.muddassar8@gmail.com>
2020-02-12 14:21:52 +0000

    Added new functionality for primenumber and sillygame

--- primeNumber.js
+++ primeNumber.js
@@ -1,3 +1,4 @@
+// Prime Number Solution 1
 const primeNumber = number => {
   let count = 0;
   for (let i = 2; i < number; i++) {
@@ -9,6 +10,17 @@ const primeNumber = number => {
 
   if (count > 0) {
     return 'Not Prime Number';
+  }
+  return 'Prime Number';
+};
+
+// Prime Number Solution 2
+const primeNumber2 = number => {
+  let count = 0;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return 'Not Prime Number';
+    }
   }
   return 'Prime Number';
 };
@@ -26,5 +38,6 @@ const primeNumberUsingRecursion = (number, index, count) => {
   }
 };
 
-console.log(primeNumber(21));
-console.log(primeNumberUsingRecursion(21, 2, 0));
+console.log(primeNumber(403));
+console.log(primeNumber2(29));
+console.log(primeNumberUsingRecursion(7, 2, 0));
